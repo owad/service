0d2164fd8bb729a5b0cf597af44702285b6339bf
Łukasz Lechowicz <llechowicz@gmail.com>
2011-11-05 20:54:56 +0000

    some changes :>

--- www/account/urls.py
+++ www/account/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
-from www.account.views import *
+from www.account.views import Profile
 
-urlpatterns = patterns('www.service.views',
+urlpatterns = patterns('account.views',
     url(r'^$', Profile.as_view(), name='profile'),
 )
