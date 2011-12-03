bdc2e8858691900e65b1f0881a488048e7667b2e
Łukasz Lechowicz <lukasz@potatolondon.com>
2011-12-03 22:38:30 +0000

    refurbishment

--- www/report/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import url, patterns
-from www.report.views import ReportView
-from django.contrib.auth.decorators import login_required
-
-urlpatterns = patterns('report.views',
-    url(r'^raporty/$', login_required(ReportView.as_view()) , name='reports'),
-)
